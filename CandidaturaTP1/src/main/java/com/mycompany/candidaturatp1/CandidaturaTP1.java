b1866b0b21559ed7255eefac315e1e3d10b7a788
Luiz <louizfelipe23@gmail.com>
2025-09-23 19:19:31 -0300

    Criei as partes visuais relacionadas a Candidatura (Cadastro de candidatos, Candidatura a vaga, Status das candidaturas)

---
+++ CandidaturaTP1/src/main/java/com/mycompany/candidaturatp1/CandidaturaTP1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.candidaturatp1;
+
+/**
+ *
+ * @author lipit
+ */
+public class CandidaturaTP1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
