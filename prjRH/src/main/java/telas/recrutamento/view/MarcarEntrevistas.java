3de47e46255f5a646bd499dc2aacd76456018d61
RaulMyron <raul.myron@gmail.com>
2025-10-29 00:40:21 -0300

    trying to fix things

--- prjRH/src/main/java/telas/recrutamento/view/MarcarEntrevistas.java
+++ prjRH/src/main/java/telas/recrutamento/view/MarcarEntrevistas.java
@@ -19,10 +19,12 @@ public class MarcarEntrevistas extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(MarcarEntrevistas.class.getName());
 
-    /**
-     * Creates new form MarcarEntrevistas
-     */
-    public MarcarEntrevistas() {
+    private VagaController vagaController;
+    private EntrevistaController entrevistaController;
+    private Recrutador recrutadorLogado;
+    private String candidaturaSelecionada;
+    
+    public MarcarEntrevistas(Recrutador recrutador) {
         initComponents();
         this.recrutadorLogado = recrutador;
         this.vagaController = new VagaController();
@@ -644,11 +646,7 @@ public class MarcarEntrevistas extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -659,10 +657,12 @@ public class MarcarEntrevistas extends javax.swing.JFrame {
         } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
             logger.log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new MarcarEntrevistas().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> {
+            Recrutador recrutadorTeste = new Recrutador("12345678900", "João Teste", "teste@empresa.com");
+            new MarcarEntrevistas(recrutadorTeste).setVisible(true);
+        });
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
